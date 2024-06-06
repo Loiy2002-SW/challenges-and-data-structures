ae3192104866730c60150397392c485b1a4dc334
loiy <lloiy9310@gmail.com>
2024-06-06 12:12:32 +0300

    ChallengeA

--- Program.cs
+++ Program.cs
@@ -4,7 +4,35 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            // Test Reveres Array method
+            string[] names = { "Loai", "Ahmad", "Basel", "Khaled"};
+
+            string[] reversedNames = ReverseArray(names);
+
+            for (int i = 0; i < reversedNames.Length; i++)
+            {
+                Console.WriteLine(reversedNames[i]);
+            }
+
+
         }
+
+        // Reveres Array method
+        static string[] ReverseArray(string[] array) { 
+        
+            string[] reversedArray = new string[array.Length];
+
+            for (int i = 0; i < array.Length; i++) { 
+            
+                reversedArray[i] = array[array.Length -1 -i];
+
+            }
+
+            return reversedArray;
+
+        }
+
+
     }
 }
