b617b8357bbeef6e112007da974f62bdf30f2e61
loiy <lloiy9310@gmail.com>
2024-06-06 11:31:14 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace challenges_and_data_structures
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
