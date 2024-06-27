3cd5a620a076ae3af4c5f07055b9815dbd831e3f
loiy <lloiy9310@gmail.com>
2024-06-27 12:37:45 +0300

    Solve the challenge 5

---
+++ Challenges/Find-Duplicates/Program.cs
@@ -0,0 +1,42 @@
+﻿
+
+namespace challenges_and_data_structures
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ints = { 1, 2, 3, 4, 5, 6, 5, 2, 3, 4, 8, 9, 7, 9, 2 , 0, 0, 0};
+
+            int[] sef = FindDuplicates(ints);
+
+            for (int i = 0; i < sef.Length; i++) {
+
+                Console.WriteLine(sef[i]);
+            }
+
+
+        }
+
+        public static int[] FindDuplicates(int[] nums)
+        {
+            Dictionary<int, int> directory = new Dictionary<int, int>();
+
+            foreach (int num in nums)
+                if (directory.ContainsKey(num))
+                    directory[num]++;
+                else
+                    directory.Add(num, 1);
+
+            List<int> duplicates = new List<int>();
+
+            foreach (var kvp in directory)
+                if (kvp.Value > 1)
+                    duplicates.Add(kvp.Key);
+
+            return duplicates.ToArray();
+        }
+
+
+    }
+}
